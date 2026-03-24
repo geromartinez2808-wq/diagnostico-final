c5539a10f73d50471d0683573d62ee650555925f
Geronimo Martinez <geronimomartinez@MacBook-Air-de-Geronimo.local>
2026-03-24 20:03:25 -0300

    Versión limpia de la web

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import OnboardingForm from './components/OnboardingForm';
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <OnboardingForm />
+    </div>
+  );
+}
